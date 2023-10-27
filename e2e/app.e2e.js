998d92cb3d7135a34a7bf258961737cceabe1abf
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-26 20:27:34 -0400

    feat: best practies

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,16 +1,26 @@
 const request = require('supertest');
 const express = require('express');
 
-const app = express();
-
-app.get('/hello', (req, res) => {
-  res.status(200).json({name: 'carlos'});
-});
-
-app.listen(3001);
-const api = request(app);
 
 describe('Test for app', () => {
+
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = express();
+
+    app.get('/hello', (req, res) => {
+      res.status(200).json({name: 'carlos'});
+    });
+
+    server = app.listen(3001);
+    api = request(app);
+  });
+
+
+
   test('GET /hello', async () => {
     const response = await api.get('/hello');
     console.log(response.text);
@@ -19,4 +29,8 @@ describe('Test for app', () => {
     expect(response.body.name).toEqual('carlos');
     expect(response.headers['content-type']).toMatch(/json/);
   })
+
+  afterEach(() => {
+    server.close();
+  });
 });
